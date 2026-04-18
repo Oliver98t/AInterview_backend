f3a974401cefb2beb390719cec5dde7850c852ca
Oliver98t <oli1998t@gmail.com>
2026-04-18 17:20:13 +0100

    made deploy script for docker and terraform

--- Deploy.sh
+++ Deploy.sh
@@ -2,7 +2,7 @@
 
 env="dev"
 account_id=$(aws sts get-caller-identity --query "Account" --output text)
-region="eu-west-2"
+region=$(aws configure get region)
 registry_url="$account_id.dkr.ecr.$region.amazonaws.com"
 commit=$(git rev-parse --short HEAD)
 
@@ -15,11 +15,16 @@ docker rmi $(docker images | grep -E "response|speechtotext" | awk '{print $2}')
 # build/tag/push Response
 response_tag="response-$env:$commit"
 docker build -t $response_tag LambdaSrc/Response/
-docker tag $response_tag $registry_url/$response_tag
+response_image_uri="$registry_url/$response_tag"
+docker tag $response_tag $response_image_uri
 docker push $registry_url/$response_tag
 
 # build/tag/push Response
 speechtotext_tag="speechtotext-$env:$commit"
 docker build -t $speechtotext_tag LambdaSrc/SpeechToText/
-docker tag $speechtotext_tag $registry_url/$speechtotext_tag
+speechtotext_image_uri=$registry_url/$speechtotext_tag
+docker tag $speechtotext_tag $speechtotext_image_uri
 docker push $registry_url/$speechtotext_tag
+
+cd infrastructure
+terraform apply -var="environment=$env" -var="Response_image_uri=$response_image_uri" -var="SpeechToText_image_uri=$speechtotext_image_uri"
