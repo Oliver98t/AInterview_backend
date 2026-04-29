a6cb4257923a1fa78be79cd4c559b68d8712c2fc
Oliver98t <oli1998t@gmail.com>
2026-04-29 18:58:17 +0100

    corrected query string params

--- lambda_src/get_response/index.py
+++ lambda_src/get_response/index.py
@@ -19,15 +19,17 @@ table = dynamo.Table(DYNAMODB_TABLE)
 
 def handler(event, context):
     logger.info(f"Event: {event}")
-
-    jobId: dict = event.get('jobId')
+    query_parameters: dict = event.get('queryStringParameters')
+    jobId = query_parameters.get("jobId")
+    logger.info(jobId)
+    
     if LOCAL_TEST != None:
         # Get a single item by primary key
         response = table.get_item(
             Key={'id': jobId}
         )
-        item = response.get('response')
         logger.info(response)
+        item = response.get('response')
         logger.info(item)
 
     return {
